f98231541ce3d19133f7a475bdc723e73aec359e
Malij <malij@divar.ir>
2017-04-05 16:37:54 +0430

    Completes clear.sh

--- cleaner.sh
+++ cleaner.sh
@@ -1,17 +1,18 @@
+# Clean List
 sed -e "s/\[.*\]//g; s/([^()]*)//g; s/ /+/g" movies.txt > out.txt
 
-echo '[' > out.json
+# Fetch data about movies and save in out.json
+rm out.json
+
+echo '[' >> out.json 
 
 while read -r line
 do
-	curl "http://www.omdbapi.com/?t="$p > out.json
-	echo ',' >  out.json
+	echo '{\"movie\": \"'$line'\", \"info\": '
+	curl "http://www.omdbapi.com/?t="$line >> out.json
+	echo ',' >>  out.json
+	
 done < out.txt
 
-#while read p; do
-#  curl "http://www.omdbapi.com/?t="$p > response
-#  echo response,','
-#done < <(out)
-
-echo "]" > out.json
+echo "]" >> out.json
 
