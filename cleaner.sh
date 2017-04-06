63e6b2095010de89a384753c567b87a8f70e39cb
Coybit <coybit@gmail.com>
2017-04-06 10:41:33 +0430

    fixes the last comma problem

--- cleaner.sh
+++ cleaner.sh
@@ -2,7 +2,10 @@
 sed -e "s/\[.*\]//g; s/([^()]*)//g; s/ /+/g" movies.txt > out.txt
 
 # Fetch data about movies and save in out.json
-rm out.json
+#rm out.json
+
+totalLines=$(grep -c '^' out.txt)
+nLines=1
 
 echo '[' >> out.json 
 
@@ -10,7 +13,15 @@ while read -r line
 do
 	echo '{"movie": "'$line'", "info": ' >> out.json
 	curl "http://www.omdbapi.com/?t="$line >> out.json
-	echo '},' >>  out.json
+
+	if [ $nLines -lt $totalLines ] 
+	then
+		echo '},' >>  out.json
+	else
+		echo '}' >>  out.json
+	fi
+
+	nLines=$(($nLines + 1))
 	
 done < out.txt
 
